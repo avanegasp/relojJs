e0400c40cf6d081d0b5393eb3b35a8d7f806e337
Angie Vanegas <arco527@gmail.com>
2017-05-26 22:31:38 -0500

    add without working

---
+++ js/app.js
@@ -0,0 +1,56 @@
+  (function(){
+    var actualizarHora = function(){
+      var fecha = new Date(),
+      horas = fecha.getHours(),
+      ampm,
+      minutos = fecha.getMinutes(),
+      segundos = fecha.getSeconds(),
+      diaSemana = fecha.getDay(),
+      dia = fecha.getDate(),
+      mes = fecha.getMonth(),
+      year = fecha.getFullYear();
+
+    var pHoras = document.getElementById("horas");
+    var pDiaSemana = document.getElementById("diaSemana");
+    var pDia = document.getElementById("dia");
+    var pMes = document.getElementById("mes");
+    var pYear = document.getElementById("year");
+    var pMinutos = document.getElementById("minutos");
+    var pSegundos = document.getElementById("segundos");
+    var pAmPm = document.getElementById("ampm");
+
+    var semana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    pDiaSemana.textContent = semana[diaSemana];
+
+    pDia.textContent = dia;
+
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    pMes.textContent = meses[mes];
+
+    pYear.textContent = year;
+
+    if(horas >= 12){
+      // horas = horas + 12;
+      ampm = 'PM'
+    }else {
+      ampm = 'AM'
+    };
+
+
+    pHoras.textContent = horas;
+    pAmPm.textContent = ampm;
+
+    if(minutos < 10){
+      minutos = "0" + minutos;
+    };
+
+    if(segundos < 10){
+      segundos = "0" + segundos;
+    };
+
+    pMinutos.textContent = minutos;
+    pSegundos.textContent = segundos;
+    };
+    actualizarHora();
+    var intervalo = setInterval(actualizarHora, 1000);
+  }())
